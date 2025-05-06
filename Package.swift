881c724031068049527f89f73d062ce688ed1969
Admin <ft.priest@gmail.com>
2025-05-06 21:43:25 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -2,19 +2,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "HelpLib",
+    name: "AppTokenKit",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         .library(
-            name: "HelpLib",
-            targets: ["HelpLib"]
+            name: "AppTokenKit",
+            targets: ["AppTokenKit"]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: "HelpLib",
+            name: "AppTokenKit",
             url: "https://github.com/smoke0030/HelpLib/releases/download/1.0.9/AppTokenKit.xcframework.zip",
             checksum: "4647ddc0795fd35a9d82a21b95b0ae0e9bc88af578b72dc433bca9eb2e1588fe"
         )
